c1a348f96368dfdb6876536b3fe30389ef2eb4d0
Stefan D <46678842+stduerre@users.noreply.github.com>
2022-11-30 12:11:22 +0100

    Initial Release (#1)
    * Add components
    
    * First working version
    
    * Add missing editor fields
    
    * Update fields and validation
    
    * Add backend validation and preparation
    
    * npm run build
    
    * Update formatting and readme
    
    Co-authored-by: sxtyfrty

--- planet4-gpch-tamaro.php
+++ planet4-gpch-tamaro.php
@@ -13,14 +13,158 @@
  * @package           create-block
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_planet4_gpch_tamaro_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type( __DIR__ . '/build', array( 'render_callback' => 'planet4_gpch_tamaro_render_callback' ) );
 }
+
 add_action( 'init', 'create_block_planet4_gpch_tamaro_block_init' );
+
+function planet4_gpch_tamaro_render_callback( $block_attributes, $content ) {
+
+	// var_dump($block_attributes);
+
+	$defaultAttributes = array();
+	$tamaroAttributes = array();
+
+	// Default Attributes
+
+	$defaultAttributes['debug'] = 'false';
+	$defaultAttributes['testMode'] = 'false';
+	$defaultAttributes['language'] = 'en';
+	$defaultAttributes['defaultPaymentType'] = 'onetime';
+	$defaultAttributes['defaultRecurringInterval'] = 'monthly';
+	$defaultAttributes['minimumCustomAmountOnetime'] = 1;
+	$defaultAttributes['amountsOnetime'] = '39,84,150,250';
+	$defaultAttributes['minimumCustomAmountRecurringMonthly'] = 2;
+	$defaultAttributes['minimumCustomAmountRecurringQuarterly'] = 6;
+	$defaultAttributes['minimumCustomAmountRecurringSemestral'] = 12;
+	$defaultAttributes['minimumCustomAmountRecurringYearly'] = 24;
+	$defaultAttributes['amountsRecurringMonthly'] = '7,10,20,50';
+	$defaultAttributes['salesforceCampaignID'] = '701090000005aTcAAI';
+	$defaultAttributes['salesforceProduct'] = 'Standard Donation';
+
+	// Tamaro Attributes
+
+	// debug
+	if ( array_key_exists('debug', $block_attributes) && $block_attributes['debug'] === true ) {
+		$tamaroAttributes['debug'] = 'true';
+	} else {
+		$tamaroAttributes['debug'] = $defaultAttributes['debug'];
+	}
+
+	// testMode
+	if ( array_key_exists('testMode', $block_attributes) && $block_attributes['testMode'] === true ) {
+		$tamaroAttributes['testMode'] = 'true';
+	} else {
+		$tamaroAttributes['testMode'] = $defaultAttributes['testMode'];
+	}
+
+	// language
+	$allowedLanguages = array( 'de', 'en', 'it' );
+	if ( array_key_exists( 'language', $block_attributes ) && in_array( $block_attributes['language'], $allowedLanguages ) ) {
+		$tamaroAttributes['language'] = $block_attributes['language'];
+	} else {
+		$tamaroAttributes['language'] = $defaultAttributes['language'];
+	}
+
+	// defaultPaymentType
+	$allowedDefaultPaymentTypes = array( 'onetime', 'recurring' );
+	if ( array_key_exists( 'defaultPaymentType', $block_attributes ) && in_array( $block_attributes['defaultRecurringInterval'], $allowedDefaultPaymentTypes ) ) {
+		$tamaroAttributes['defaultPaymentType'] = $block_attributes['defaultPaymentType'];
+	} else {
+		$tamaroAttributes['defaultPaymentType'] = $defaultAttributes['defaultPaymentType'];
+	}
+
+	// defaultRecurringInterval
+	$allowedDefaultRecurringIntervals = array( 'monthly', 'quarterly', 'semestral', 'yearly' );
+	if ( array_key_exists( 'defaultRecurringInterval', $block_attributes ) && in_array( $block_attributes['defaultRecurringInterval'], $allowedDefaultRecurringIntervals ) ) {
+		$tamaroAttributes['defaultRecurringInterval'] = $block_attributes['defaultRecurringInterval'];
+	} else {
+		$tamaroAttributes['defaultRecurringInterval'] = $defaultAttributes['defaultRecurringInterval'];
+	}
+
+	// minimumCustomAmountOnetime
+	if ( array_key_exists( 'minimumCustomAmountOnetime', $block_attributes ) && is_integer( $block_attributes['minimumCustomAmountOnetime'] ) ) {
+		$tamaroAttributes['minimumCustomAmountOnetime'] = $block_attributes['minimumCustomAmountOnetime'];
+	} else {
+		$tamaroAttributes['minimumCustomAmountOnetime'] = $defaultAttributes['minimumCustomAmountOnetime'];
+	}
+
+	// amountsOnetime
+	if ( array_key_exists( 'amountsOnetime', $block_attributes ) && is_string( $block_attributes['amountsOnetime'] ) && ( ! empty( $block_attributes['amountsOnetime'] ) ) ) {
+		$tamaroAttributes['amountsOnetime'] = $block_attributes['amountsOnetime'];
+	} else {
+		$tamaroAttributes['amountsOnetime'] = $defaultAttributes['amountsOnetime'];
+	}
+
+	// minimumCustomAmountRecurringMonthly
+	if ( array_key_exists( 'minimumCustomAmountRecurringMonthly', $block_attributes ) && is_integer( $block_attributes['minimumCustomAmountRecurringMonthly'] ) ) {
+		$minimumCustomAmountRecurringMonthly                       = $block_attributes['minimumCustomAmountRecurringMonthly'];
+		$tamaroAttributes['minimumCustomAmountRecurringMonthly']   = $minimumCustomAmountRecurringMonthly;
+		$tamaroAttributes['minimumCustomAmountRecurringQuarterly'] = $minimumCustomAmountRecurringMonthly * 3;
+		$tamaroAttributes['minimumCustomAmountRecurringSemestral'] = $minimumCustomAmountRecurringMonthly * 6;
+		$tamaroAttributes['minimumCustomAmountRecurringYearly']    = $minimumCustomAmountRecurringMonthly * 12;
+	} else {
+		$tamaroAttributes['minimumCustomAmountRecurringMonthly']   = $defaultAttributes['minimumCustomAmountRecurringMonthly'];
+		$tamaroAttributes['minimumCustomAmountRecurringQuarterly'] = $defaultAttributes['minimumCustomAmountRecurringQuarterly'];
+		$tamaroAttributes['minimumCustomAmountRecurringSemestral'] = $defaultAttributes['minimumCustomAmountRecurringSemestral'];
+		$tamaroAttributes['minimumCustomAmountRecurringYearly']    = $defaultAttributes['minimumCustomAmountRecurringYearly'];
+	}
+
+	// amountsRecurringMonthly
+	if ( array_key_exists( 'amountsRecurringMonthly', $block_attributes ) && is_string( $block_attributes['amountsRecurringMonthly'] ) && ( ! empty( $block_attributes['amountsRecurringMonthly'] ) ) ) {
+		$amountsRecurringMonthly                       = $block_attributes['amountsRecurringMonthly'];
+		$tamaroAttributes['amountsRecurringMonthly']   = $amountsRecurringMonthly;
+		$tamaroAttributes['amountsRecurringQuarterly'] = multiplyAmounts( $amountsRecurringMonthly, 3 );
+		$tamaroAttributes['amountsRecurringSemestral'] = multiplyAmounts( $amountsRecurringMonthly, 6 );
+		$tamaroAttributes['amountsRecurringYearly']    = multiplyAmounts( $amountsRecurringMonthly, 12 );
+	} else {
+		$defaultAmountsRecurringMonthly                = $defaultAttributes['amountsRecurringMonthly'];
+		$tamaroAttributes['amountsRecurringMonthly']   = $defaultAmountsRecurringMonthly;
+		$tamaroAttributes['amountsRecurringQuarterly'] = multiplyAmounts( $defaultAmountsRecurringMonthly, 3 );
+		$tamaroAttributes['amountsRecurringSemestral'] = multiplyAmounts( $defaultAmountsRecurringMonthly, 6 );
+		$tamaroAttributes['amountsRecurringYearly']    = multiplyAmounts( $defaultAmountsRecurringMonthly, 12 );
+	}
+
+	// salesforceCampaignID
+	if ( array_key_exists( 'salesforceCampaignID', $block_attributes ) && is_string( $block_attributes['minimumCustomAmountOnetime'] ) && ( ! empty( $block_attributes['minimumCustomAmountOnetime'] ) ) ) {
+		$tamaroAttributes['salesforceCampaignID'] = $block_attributes['salesforceCampaignID'];
+	} else {
+		$tamaroAttributes['salesforceCampaignID'] = $defaultAttributes['salesforceCampaignID'];
+	}
+
+	// salesforceProduct
+	$allowedSalesforceProducts = array(
+		'Standard Donation',
+		'Sponsorship Agriculture',
+		'Sponsorship Climate',
+		'Sponsorship Fleet',
+		'Sponsorship Forest',
+		'Sponsorship Ocean'
+	);
+	if ( array_key_exists( 'salesforceProduct', $block_attributes ) && in_array( $block_attributes['salesforceProduct'], $allowedSalesforceProducts ) ) {
+		$tamaroAttributes['salesforceProduct'] = $block_attributes['salesforceProduct'];
+	} else {
+		$tamaroAttributes['salesforceProduct'] = $defaultAttributes['salesforceProduct'];
+	}
+
+	// prepare parameters for template
+	$params = array( 'tamaroAttributes' => $tamaroAttributes );
+
+	// output template
+	return \Timber::fetch( __DIR__ . '/templates/tamaro.twig', $params );
+}
+
+function multiplyAmounts (string $commaSeparatedAmounts, int $multiplicator)
+{
+	$amounts = explode(',', $commaSeparatedAmounts);
+	$multipliedAmounts = array();
+
+	foreach ($amounts as $amount) {
+		$multipliedAmounts[] = $amount * $multiplicator;
+	}
+
+	$multipliedCommaSeparatedAmounts = implode(',', $multipliedAmounts);
+
+	return $multipliedCommaSeparatedAmounts;
+}
--- src/edit.js
+++ src/edit.js
@@ -1,37 +1,144 @@
-/**
- * WordPress components that create the necessary UI elements for the block
- *
- * @see https://developer.wordpress.org/block-editor/packages/packages-components/
- */
-import { TextControl } from '@wordpress/components';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
+import {
+	TextControl,
+	SelectControl,
+	CheckboxControl,
+} from '@wordpress/components';
 import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @param {Object}   props               Properties passed to the function.
- * @param {Object}   props.attributes    Available block attributes.
- * @param {Function} props.setAttributes Function that updates individual attributes.
- *
- * @return {WPElement} Element to render.
- */
 export default function Edit( { attributes, setAttributes } ) {
-	const blockProps = useBlockProps();
 	return (
-		<div { ...blockProps }>
+		<div { ...useBlockProps() }>
+			<CheckboxControl
+				label="Debug"
+				checked={ attributes.debug }
+				onChange={ ( val ) => setAttributes( { debug: val } ) }
+			/>
+			<CheckboxControl
+				label="Test Mode"
+				checked={ attributes.testMode }
+				onChange={ ( val ) => setAttributes( { testMode: val } ) }
+			/>
+			<br />
+			<SelectControl
+				label="Language"
+				value={ attributes.language }
+				options={ [
+					{
+						label: 'Select an option ...',
+						value: '',
+						disabled: false,
+					},
+					{ label: 'German', value: 'de' },
+					{ label: 'French', value: 'fr' },
+					{ label: 'Italian', value: 'it' },
+				] }
+				onChange={ ( val ) => setAttributes( { language: val } ) }
+			/>
+			<SelectControl
+				label="Default Payment Type"
+				value={ attributes.defaultPaymentType }
+				options={ [
+					{
+						label: 'Select an option ...',
+						value: '',
+						disabled: false,
+					},
+					{ label: 'Onetime', value: 'onetime' },
+					{ label: 'Recurring', value: 'recurring' },
+				] }
+				onChange={ ( val ) =>
+					setAttributes( { defaultPaymentType: val } )
+				}
+			/>
+			<SelectControl
+				label="Default Recurring Interval"
+				value={ attributes.defaultRecurringInterval }
+				options={ [
+					{
+						label: 'Select an option ...',
+						value: '',
+						disabled: false,
+					},
+					{ label: 'Monthly', value: 'monthly' },
+					{ label: 'Quarterly', value: 'quarterly' },
+					{ label: 'Semestral', value: 'semestral' },
+					{ label: 'Yearly', value: 'yearly' },
+				] }
+				onChange={ ( val ) =>
+					setAttributes( { defaultRecurringInterval: val } )
+				}
+			/>
+
 			<TextControl
-				value={ attributes.message }
-				onChange={ ( val ) => setAttributes( { message: val } ) }
+				label="Minimum Custom Amount Onetime"
+				value={ attributes.minimumCustomAmountOnetime }
+				onChange={ ( val ) =>
+					setAttributes( {
+						minimumCustomAmountOnetime: parseInt( val ),
+					} )
+				}
+				help="Minimum custom amount which can be set on the form for onetime donations."
+			/>
+			<TextControl
+				label="Amounts Onetime"
+				value={ attributes.amountsOnetime }
+				onChange={ ( val ) => setAttributes( { amountsOnetime: val } ) }
+				help="Predefined amounts which appear on the form for onetime donations (comma separated list, example: 39,84,150,250)."
+			/>
+			<TextControl
+				label="Minimum Custom Amount Recurring (monthly)"
+				value={ attributes.minimumCustomAmountRecurringMonthly }
+				onChange={ ( val ) =>
+					setAttributes( {
+						minimumCustomAmountRecurringMonthly: parseInt( val ),
+					} )
+				}
+				help="Minimum custom amount which can be set on the form for monthly recurring donations."
+			/>
+			<TextControl
+				label="Amounts Recurring (monthly)"
+				value={ attributes.amountsRecurringMonthly }
+				onChange={ ( val ) =>
+					setAttributes( { amountsRecurringMonthly: val } )
+				}
+				help="Predefined amounts which appear on the form for monthly recurring donations (comma separated list, example: 7,10,20,50)."
+			/>
+
+			<TextControl
+				label="Salesforce Campaign ID"
+				value={ attributes.salesforceCampaignID }
+				onChange={ ( val ) =>
+					setAttributes( { salesforceCampaignID: val } )
+				}
+			/>
+			<SelectControl
+				label="Salesforce Product"
+				value={ attributes.salesforceProduct }
+				options={ [
+					{
+						label: 'Select an option ...',
+						value: '',
+						disabled: false,
+					},
+					{ label: 'Standard Donation', value: 'Standard Donation' },
+					{
+						label: 'Sponsorship Agriculture',
+						value: 'Sponsorship Agriculture',
+					},
+					{
+						label: 'Sponsorship Climate',
+						value: 'Sponsorship Climate',
+					},
+					{ label: 'Sponsorship Fleet', value: 'Sponsorship Fleet' },
+					{
+						label: 'Sponsorship Forest',
+						value: 'Sponsorship Forest',
+					},
+					{ label: 'Sponsorship Ocean', value: 'Sponsorship Ocean' },
+				] }
+				onChange={ ( val ) =>
+					setAttributes( { salesforceProduct: val } )
+				}
 			/>
 		</div>
 	);
