d82fd17b4cb510469987e5c6a774b8f22915c957
sxtyfrty <carndt@greenpeace.org>
2022-12-04 00:00:37 +0100

    Add some help information and styling

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f7f61052dbc651526fe7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '674b1b83a07c577494a2');
