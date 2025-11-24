df22be579da7176b9a02c55fc02be0e5463e1cbb
Stefan Dürrenberger <sd@pm7.ch>
2025-11-24 15:56:03 +0100

    ES-221: build scripts/css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '2624e66c7e1e0340ad31');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '37c6d60901729cebcdc6');
